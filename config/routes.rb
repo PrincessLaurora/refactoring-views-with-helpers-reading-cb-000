2497ea02340a8c814650df7ddd00afc8c0e02d5d
Laura Felce <crepu83@yahoo.it>
2019-06-24 10:59:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  get 'authors/show'
+  
 
   get 'test/index'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :authors, only: [:index, :show]
 end
