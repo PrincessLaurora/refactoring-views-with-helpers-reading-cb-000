5abe323d903f80d183a796d7e0408478b7b5e089
Laura Felce <crepu83@yahoo.it>
2019-06-24 11:04:53 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,11 +3,4 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def new
-    @author = Author.new
-  end
-
-  def index
-		@authors = Author.all
-	end
 end
