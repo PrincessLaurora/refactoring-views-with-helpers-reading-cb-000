89e85c5d913db37e9e3b42beeef05304d39ab8d8
Laura Felce <crepu83@yahoo.it>
2019-06-24 10:54:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'test/index'
 
-  resorces :author, only:[:show]
+  resources :author, only:[:show]
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
